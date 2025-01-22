1585b00e3e32091224ffb4d6d91fcf2fb78e98fc
Yonatan Sponaro <yoni200568@gmail.com>
2025-01-22 22:49:08 +0200

    base update

---
+++ Assets/scripts/Health/Health.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+    [Header("Health")]
+    [SerializeField] public float startingHealth;
+    [SerializeField] private MonoBehaviour movementScript;
+    public float currentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
+
+    [Header("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    [SerializeField] Color flashColor;
+    private SpriteRenderer spriteRend;
+
+    private void Awake()
+    {
+        dead = false;
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+
+        if (currentHealth > 0)
+        {
+            anim.SetTrigger("hurt");
+            if (iFramesDuration > 0)
+                StartCoroutine(Invunerability());
+        }
+        else
+        {
+            if (!dead)
+            {
+                anim.SetTrigger("death");
+                movementScript.enabled = false;
+                dead = true;
+            }
+        }
+    }
+
+    public void AddHealth(float _value)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+    private IEnumerator Invunerability()
+    {
+        Physics2D.IgnoreLayerCollision(8, 9, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = flashColor;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(8, 9, false);
+    }
+}
--- Assets/scripts/enemies/DamageDealer.cs
+++ Assets/scripts/enemies/DamageDealer.cs
@@ -5,7 +5,6 @@ public class DamageDealer : MonoBehaviour
     [SerializeField] private float damage;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // add hachala
         if (collision.tag == "Player")
             collision.GetComponent<Health>().TakeDamage(damage);
     }
